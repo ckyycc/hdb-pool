766d69902f8553726f7a48c743921184d2bdcc19
CK <kuang.cheng@sap.com>
2020-03-06 19:11:48 -0800

    support spread operator

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     "extends": "standard",
     "parserOptions": {
-        "ecmaVersion": 2017
+        "ecmaVersion": 2018
     },
     "rules": {
         'max-len': ["error", { "code": 140 }],
